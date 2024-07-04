eaf73f03744c125f755a8fec13a7524bc6718be9
Shcoobz <a.boeckmann23@gmail.com>
2024-07-04 16:10:05 +0200

    setup basic backend server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,24 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log(`\nServer running on port ${PORT}\n`));
+app.use('/', require('./routes/root'));
+
+app.all('*', (req, res) => {
+  res.status(404);
+
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not found' });
+  } else {
+    res.type('txt').send('404 Not found');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`\nServer running on port ${PORT}\nVisit: http://localhost:${PORT}\n`)
+);
