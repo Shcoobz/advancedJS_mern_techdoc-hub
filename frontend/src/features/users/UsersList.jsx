71fae2df086ed632a36567e4530e783672a393e5
Shcoobz <a.boeckmann23@gmail.com>
2024-07-08 16:59:11 +0200

    added options for getters users/notes to make sure data is refetched at specific interval

--- frontend/src/features/users/UsersList.jsx
+++ frontend/src/features/users/UsersList.jsx
@@ -2,7 +2,17 @@ import { useGetUsersQuery } from './usersApiSlice';
 import User from './User';
 
 function UsersList() {
-  const { data: users, isLoading, isSuccess, isError, error } = useGetUsersQuery();
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery(undefined, {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
 
   let content;
 
