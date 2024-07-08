00d336e4040d4a35fda568a51703fe00e81ef059
Shcoobz <a.boeckmann23@gmail.com>
2024-07-08 11:31:17 +0200

    implemented api && user api

--- frontend/src/components/Public.jsx
+++ frontend/src/components/Public.jsx
@@ -5,12 +5,12 @@ function Public() {
     <section className='public'>
       <header>
         <h1>
-          Welcome to <span className='nowrap'>Company Name!</span>
+          Welcome to <span className='nowrap'>[ Company Name ]!</span>
         </h1>
       </header>
       <main className='public__main'>
         <h2>
-          <i>Company Description:</i>
+          <i>[ Company Description ]:</i>
         </h2>
         <br />
         <p>
@@ -43,52 +43,21 @@ function Public() {
           est amet pariatur quis do exercitation.
         </p>
         <br />
-        <p>
-          Tempor dolor nulla deserunt in dolore elit. In eu ut sunt laborum occaecat
-          exercitation amet sunt ex cillum aliqua cillum mollit. Proident officia id
-          laboris commodo incididunt nulla et amet. Ipsum excepteur eiusmod sint dolor
-          pariatur consectetur nisi in. Nulla nulla culpa ut anim aliqua ea reprehenderit
-          irure qui. Mollit consequat elit voluptate sit et elit pariatur labore occaecat.
-          Duis sint dolore nulla Lorem in. Consectetur culpa ipsum enim sunt nulla
-          cupidatat ad ea. Culpa id velit aliquip quis do minim aute mollit ullamco
-          adipisicing officia velit.In exercitation quis dolore aliquip incididunt.
-          Ullamco cupidatat elit quis amet laborum. Ipsum culpa non occaecat ullamco
-          exercitation cillum eiusmod eiusmod aute ullamco mollit. Exercitation ut sint
-          nulla laboris anim sit mollit eiusmod ex velit commodo et ex minim. Dolor Lorem
-          est amet pariatur quis do exercitation.
-        </p>
-        <br />
-        <p>
-          Tempor dolor nulla deserunt in dolore elit. In eu ut sunt laborum occaecat
-          exercitation amet sunt ex cillum aliqua cillum mollit. Proident officia id
-          laboris commodo incididunt nulla et amet. Ipsum excepteur eiusmod sint dolor
-          pariatur consectetur nisi in. Nulla nulla culpa ut anim aliqua ea reprehenderit
-          irure qui. Mollit consequat elit voluptate sit et elit pariatur labore occaecat.
-          Duis sint dolore nulla Lorem in. Consectetur culpa ipsum enim sunt nulla
-          cupidatat ad ea. Culpa id velit aliquip quis do minim aute mollit ullamco
-          adipisicing officia velit.In exercitation quis dolore aliquip incididunt.
-          Ullamco cupidatat elit quis amet laborum. Ipsum culpa non occaecat ullamco
-          exercitation cillum eiusmod eiusmod aute ullamco mollit. Exercitation ut sint
-          nulla laboris anim sit mollit eiusmod ex velit commodo et ex minim. Dolor Lorem
-          est amet pariatur quis do exercitation.
-        </p>
-
-        <br />
 
         <address className='public__addr'>
           <br />
-          <h2>Company Details:</h2>
+          <h2>[ Company Details ]:</h2>
           <br />
-          Company Name
+          [ Company Name ]
           <br />
-          Street
+          [ Street ]
           <br />
-          City
+          [ City ]
           <br />
-          <a href='tel:+431234567890'>Phone Number</a>
+          <a href='tel:+431234567890'>[ Phone Number ]</a>
         </address>
         <br />
-        <p>Owner Name</p>
+        <p>[ Owner Name ]</p>
       </main>
       <footer>
         <Link to='/login'>Employee Login</Link>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
 import App from './App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
