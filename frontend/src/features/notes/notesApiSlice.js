dced6de1916cff2480dbd3526bf9c8711fb08db5
Shcoobz <a.boeckmann23@gmail.com>
2024-07-10 19:50:38 +0200

    added validateStatus to query

--- frontend/src/features/notes/notesApiSlice.js
+++ frontend/src/features/notes/notesApiSlice.js
@@ -2,7 +2,8 @@ import { createSelector, createEntityAdapter } from '@reduxjs/toolkit';
 import { apiSlice } from '../../app/api/apiSlice';
 
 const notesAdapter = createEntityAdapter({
-  sortComparer: (a, b) => (a.completed === b.completed ? 0 : a.completed ? 1 : -1),
+  sortComparer: (a, b) =>
+    a.completed === b.completed ? 0 : a.completed ? 1 : -1,
 });
 
 const initialState = notesAdapter.getInitialState();
@@ -10,10 +11,12 @@ const initialState = notesAdapter.getInitialState();
 export const notesApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getNotes: builder.query({
-      query: () => '/notes',
-      validateStatus: (response, result) => {
-        return response.status === 200 && !result.isError;
-      },
+      query: () => ({
+        url: '/notes',
+        validateStatus: (response, result) => {
+          return response.status === 200 && !result.isError;
+        },
+      }),
       transformResponse: (responseData) => {
         const loadedNotes = responseData.map((note) => {
           note.id = note._id;
@@ -84,4 +87,6 @@ export const {
   selectById: selectNoteById,
   selectIds: selectNoteIds,
   // Pass in a selector that returns the notes slice of state
-} = notesAdapter.getSelectors((state) => selectNotesData(state) ?? initialState);
+} = notesAdapter.getSelectors(
+  (state) => selectNotesData(state) ?? initialState
+);
