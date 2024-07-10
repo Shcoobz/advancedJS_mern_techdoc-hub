32341a057f9fbf529ce536ddbc2ef4506b397464
Shcoobz <a.boeckmann23@gmail.com>
2024-07-10 20:03:12 +0200

    adapted component and implemented memo

--- frontend/src/features/notes/Note.jsx
+++ frontend/src/features/notes/Note.jsx
@@ -2,11 +2,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
-import { selectNoteById } from './notesApiSlice';
+import { useGetNotesQuery } from './notesApiSlice';
+import { memo } from 'react';
 
 function Note({ noteId }) {
-  const note = useSelector((state) => selectNoteById(state, noteId));
+  const { note } = useGetNotesQuery('notesList', {
+    selectFromResult: ({ data }) => ({
+      note: data?.entities[noteId],
+    }),
+  });
 
   const navigate = useNavigate();
 
@@ -55,4 +59,6 @@ function Note({ noteId }) {
   } else return null;
 }
 
-export default Note;
+const memoizedNote = memo(Note);
+
+export default memoizedNote;
