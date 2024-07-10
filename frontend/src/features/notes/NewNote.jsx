6e1d1e53ca082e38ce87f4c314eb1a2786c2c1a8
Shcoobz <a.boeckmann23@gmail.com>
2024-07-10 20:19:48 +0200

    added extra layer of security

--- frontend/src/features/notes/NewNote.jsx
+++ frontend/src/features/notes/NewNote.jsx
@@ -1,9 +1,12 @@
 import { useGetUsersQuery } from '../users/usersApiSlice';
 import PulseLoader from 'react-spinners/PulseLoader';
+import useAuth from '../../hooks/useAuth';
 
 import NewNoteForm from './NewNoteForm';
 
 function NewNote() {
+  const { isManager, isAdmin } = useAuth();
+
   const { users } = useGetUsersQuery('usersList', {
     selectFromResult: ({ data }) => ({
       users: data?.ids.map((id) => data?.entities[id]),
@@ -11,6 +14,10 @@ function NewNote() {
   });
 
   if (!users?.length) return <PulseLoader color={'#FFF'} />;
+
+  if (!isManager && !isAdmin) {
+    return <p className='errmsg'>No access</p>; 
+  }
 
   const content = <NewNoteForm users={users} />;
 
