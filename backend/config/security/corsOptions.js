a224953d38d271d571f1ac03cca853f80c4f962b
Shcoobz <a.boeckmann23@gmail.com>
2024-07-12 21:10:02 +0200

    removed option to access by ie postman for deployment

--- backend/config/security/corsOptions.js
+++ backend/config/security/corsOptions.js
@@ -3,7 +3,7 @@ import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== INDEX.NOT_FOUND || !origin) {
+    if (allowedOrigins.indexOf(origin) !== INDEX.NOT_FOUND) {
       callback(null, true);
     } else {
       callback(new Error(MSG.CORS.ERROR));
