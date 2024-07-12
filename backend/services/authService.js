f47c94a001e7bf84b43e5b97530aec54b531080b
Shcoobz <a.boeckmann23@gmail.com>
2024-07-12 16:34:38 +0200

    refactored authService

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,5 +1,31 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import { CONFIG } from '../config/common/constants.js';
+
+export function createAccessToken(user) {
+  return jwt.sign(
+    {
+      UserInfo: {
+        username: user.username,
+        roles: user.roles,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: CONFIG.TOKEN.ACCESS.EXPIRES_IN }
+  );
+}
+
+export function createRefreshToken(username) {
+  return jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: CONFIG.TOKEN.REFRESH.EXPIRES_IN,
+  });
+}
+
+export const verifyToken = asyncHandler(async (token, secret, callback) => {
+  jwt.verify(token, secret, callback);
+});
 
 export async function findActiveUserByUsername(username) {
   const user = await User.findOne({ username }).exec();
