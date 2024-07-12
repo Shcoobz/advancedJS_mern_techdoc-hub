e5e5bc05267612ce6c2806b49b0b22f28861b513
Shcoobz <a.boeckmann23@gmail.com>
2024-07-12 20:29:19 +0200

    refactored root routes and all other

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,11 +2,12 @@ import express from 'express';
 import path from 'path';
 
 import { __dirname } from '../config/common/utils.js';
+import { CONFIG } from '../config/common/constants.js';
 
 const router = express.Router();
 
 router.get('^/$|/index(.html)?', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'views', 'index.html'));
+  res.sendFile(path.join(__dirname, ...CONFIG.PATH.VIEWS_DIR, CONFIG.PATH.INDEX_HTML));
 });
 
 export default router;
